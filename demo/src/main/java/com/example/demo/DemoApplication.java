e645f09e815e5e01e450d2b1624e34b1f9fe508a
Ahmed.Elqayas <ahmed.elqayas@transflo.com>
2026-03-15 12:05:36 +0200

    Add entities (User, Address, Profile, Tag) and implementing one to many relationship between User and Address.

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
+import com.example.demo.entities.Address;
+import com.example.demo.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+
+		User user = User.builder()
+				.name("ahmed")
+				.email("a@b.com")
+				.password("123456")
+				.build();
+
+		Address address = Address.builder()
+				.city("city")
+				.street("street")
+				.zip("zip")
+				.state("state")
+				.build();
+
+		user.addAddress(address);
+
+		System.out.println(user);
+
 	}
 
 }
---
+++ demo/src/main/java/com/example/demo/entities/Profile.java
@@ -0,0 +1,28 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+@Getter
+@Setter
+@Entity
+@Table(name = "profiles")
+public class Profile {
+    @Id
+    @Column(name = "id")
+    private long id;
+    @Column(name = "bio")
+    private String bio;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints;
+}
