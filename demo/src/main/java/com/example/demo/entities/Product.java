a5a305d2055b5d42dea9b4b2d3886633251d86cc
Ahmed.Elqayas <ahmed.elqayas@transflo.com>
2026-03-16 12:46:50 +0200

    add wishlist table, entity, and implement the relationship between product and user entities.

--- demo/src/main/java/com/example/demo/entities/Product.java
+++ demo/src/main/java/com/example/demo/entities/Product.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
--- demo/src/main/java/com/example/demo/entities/User.java
+++ demo/src/main/java/com/example/demo/entities/User.java
@@ -40,6 +40,13 @@ public class User {
     private Set<Tag> tags = new HashSet<>();
     @OneToOne(mappedBy = "user")
     private Profile profile;
+    @ManyToMany
+    @JoinTable(
+            name = "wishlist",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private Set<Product> products = new HashSet<>();
 
     public void addAddress(Address address) {
         addresses.add(address);
